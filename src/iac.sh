d29b5597eed3312df6eaa57d8f4deb96e25e8499
Cleber Moretti <100326566+mcleber@users.noreply.github.com>
2022-08-20 20:38:25 -0300

    Correção no parâmetro OpenSSL

--- src/iac.sh
+++ src/iac.sh
@@ -19,26 +19,25 @@ groupadd GRP_SEC
 echo "--------------------------"
 echo "*Criando Usuários"
 
-#useradd carlos -c "Carlos User1" -m -s /bin/bash -p $(openssl passwd -aixmd5 Senha123) -G GRP_ADM
-useradd carlos -c "Carlos User1" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
 passwd carlos -e #Solicita troca da senha
-useradd maria -c "Maria User2" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd maria -c "Maria" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
 passwd maria -e
-useradd joao -c "João User3" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd joao -c "João" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
 passwd joao -e
 
-useradd debora -c "Debora User4" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd debora -c "Debora" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
 passwd debora -e
-useradd sebastiana -c "Sebastiana User5" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
 passwd sebastiana -e
-useradd roberto -c "Roberto User6"-m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd roberto -c "Roberto"-m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
 passwd roberto -e
 
-useradd josefina -c "Josefina User7" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd josefina -c "Josefina" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 passwd josefina -e
-useradd amanda -c "Amanda User8" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 passwd amanda -e
-useradd rogerio -c "Rogerio User9"-m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd rogerio -c "Rogerio"-m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 passwd rogerio -e
 
 echo "--------------------------"
